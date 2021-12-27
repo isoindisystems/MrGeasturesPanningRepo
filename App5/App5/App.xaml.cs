ea18d69b5eaff75a8a25e4bb20617d7d63eb8c3b
Nico Haslberger <nico.haslberger@indi.systems>
2021-12-27 20:26:24 +0100

    Added sample project

---
+++ App5/App5/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using App5.Mobile.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new testpage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App5/App5/Views/testpage.cs
@@ -0,0 +1,37 @@
+﻿using MR.Gestures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App5.Mobile.Pages
+{
+    public class testpage : Xamarin.Forms.ContentPage
+    {
+        public testpage()
+        {
+            var stacklayout = new MR.Gestures.StackLayout();
+            stacklayout.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            for (int i = 0; i < 12; i++)
+            { 
+                var button = new Xamarin.Forms.Label();
+                button.HeightRequest = 50;
+                button.Text = i.ToString();
+                button.BackgroundColor = Color.Red;
+              
+                stacklayout.Children.Add(button);
+
+            }
+
+            stacklayout.Panning += Stacklayout_Panning;
+
+            Content = stacklayout;
+
+        }
+
+        private void Stacklayout_Panning(object sender, PanEventArgs e)
+        {
+        }
+    }
+}
